2747c991da085ce0e968f718f0d8438a8f8a4fd1
fsxchen <fsxchen@gmail.com>
2016-01-06 17:05:22 +0800

    fsxchen

---
+++ file.js
@@ -0,0 +1,5 @@
+var fs= require("fs");
+ 
+fs.exists('/etc/passwd', function (exists) {
+  console.log(exists ? "存在" : "不存在!");
+});
---
+++ hello.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+
+exports.hello=function(){
+    console.log('Hello.');
+};
